516b585cbdce65c4777b9ec120dc0a6849ad278e
Manokarran <manokarranmca@gmail.com>
2024-02-25 16:40:19 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Seed Project - Mano
         </a>
       </header>
     </div>
